f7a8c24cbcc8c7be66d0aacbdf43cee4ab6900ef
Matthias Max <matthias.max@bitflower.net>
2015-01-21 15:25:38 +0100

    Template included

--- src/accordion/accordion.js
+++ src/accordion/accordion.js
@@ -49,7 +49,7 @@ angular.module('ui.bootstrap.accordion', ['ui.bootstrap.collapse'])
     controller:'AccordionController',
     transclude: true,
     replace: false,
-    templateUrl: 'template/accordion/accordion.html'
+    template: '<div class="panel-group" ng-transclude></div>'
   };
 })
 
@@ -60,7 +60,16 @@ angular.module('ui.bootstrap.accordion', ['ui.bootstrap.collapse'])
     restrict:'EA',
     transclude:true,              // It transcludes the contents of the directive into the template
     replace: true,                // The element containing the directive will be replaced with the template
-    templateUrl:'template/accordion/accordion-group.html',
+    template:'<div class="panel panel-default">' +
+                '<div class="panel-heading">' +
+                '<h4 class="panel-title">' +
+                '<a href class="accordion-toggle" ng-click="toggleOpen()" accordion-transclude="heading"><span ng-class="{\'text-muted\': isDisabled}">{{heading}}</span></a>' +
+                '</h4>' +
+                '</div>' +
+                '<div class="panel-collapse" collapse="!isOpen">' +
+                '<div class="panel-body" ng-transclude></div>' +
+                '</div>' +
+                '</div>',
     scope: {
       heading: '@',               // Interpolate the heading attribute onto this scope
       isOpen: '=?',
